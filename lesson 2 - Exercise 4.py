34af58b3cafb3c8141c29facd29b02bf6c7752bd
Smflores906 <35816813+Smflores906@users.noreply.github.com>
2018-02-07 13:37:56 -0700

    Update lesson 2 - Exercise 4.py

--- lesson 2 - Exercise 4.py
+++ lesson 2 - Exercise 4.py
@@ -1,9 +1,34 @@
-#Sebastian Flores - Lesson 2 - Exercise 3
-"""
-Write a program to prompt the user for hours and rate per hour to compute gross pay.
+#Sebastian Flores - Lesson 2 - Exercise 4
 
-Enter Hours: 35
-Enter Rate: 2.75
-Pay: 96.25
-We won't worry about making sure our pay has exactly two digits after the decimal place for now. If you want, you can play with the built-in Python round function to properly round the resulting pay to two decimal places.
+"""
+Please copy the link to your program and attach it for turn in. 
+Assume that we execute the following assignment statements:
+
+width = 17
+height = 12.0
+
+For each of the following expressions, 
+write the value of the expression and the type (of the value of the expression).
+
+width//2
+
+width/2.0
+
+height/3
+
+1 + 2 \* 5
+
+Use the Python interpreter to check your answers.
+"""
+"""
+width = 17 
+height = float(12.0)
+
+print(width//2)  = 8
+
+print(width/2.0) = 8.5
+
+print(height/3) = 4.0
+
+1 + 2 \* 5 = 11
 """
